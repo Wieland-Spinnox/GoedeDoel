feffd0ea46041227e05e7c3224478404599b8883
Wieland-Spinnox <wielandsp@gmail.com>
2025-07-08 09:24:56 +0200

    5.4

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -19,7 +19,7 @@ class GoedeDoelTest {
 	void getNaam() {
 		assertThat(doel.getNaam()).isEqualTo(NAAM);
 	}
-	
+
 	@Test
 	void eenNieuwDoelHeeftNogGeenOpbrengst(){
 		assertThat(doel.getOpbrengst()).isZero();
